5def8105af71d50b802d1b70c0ce4558ff46806e
emanuelpna <emanuelpna@yahoo.com.br>
2025-05-20 22:25:43 -0300

    use env vars to store the google search api key and engine id

--- src/infra/services/recipeSearchService.js
+++ src/infra/services/recipeSearchService.js
@@ -1,15 +1,15 @@
 // verificar uma API de busca de receitas 
-const GOOGLE_API_KEY = 'CHAVE_API';
-const SEARCH_ENGINE_ID = 'SEARCH_ENGINE_ID';
+const GOOGLE_API_KEY = process.env.EXPO_PUBLIC_GOOGLE_API_KEY;
+const SEARCH_ENGINE_ID = process.env.EXPO_PUBLIC_SEARCH_ENGINE_ID;
 
 export async function searchRecipes(ingredient) {
     try {
         const query = `${ingredient} receitas`;
         const url = `https://www.googleapis.com/customsearch/v1?key=${GOOGLE_API_KEY}&cx=${SEARCH_ENGINE_ID}&q=${encodeURIComponent(query)}`;
-        
+
         const response = await fetch(url);
         const data = await response.json();
-        
+
         // Filtra apenas resultados relevantes
         return data.items?.map(item => ({
             title: item.title,
